546181a489cab11cbb556bb05d5dfe53c4d1fda4
kk_boomer <fking.dmitry@gmail.com>
2022-07-23 14:52:20 +0200

    Начальная верстка

--- lib/widgets/notes/notes.dart
+++ lib/widgets/notes/notes.dart
@@ -6,6 +6,21 @@ class NotesWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Записи")),
+      body: ListView.separated(
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text('item $index'),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
+          itemCount: 100),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
