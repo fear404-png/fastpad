b465550bb9b5929d4e880e98225e83ee92d80b84
kk_boomer <fking.dmitry@gmail.com>
2022-07-23 18:59:41 +0200

    Верстка записей

--- lib/widgets/app/app.dart
+++ lib/widgets/app/app.dart
@@ -8,6 +8,11 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: const ColorScheme.dark().copyWith(
+          secondary: Colors.redAccent,
+        ),
+      ),
       routes: {"/notes": (context) => const NotesWidget()},
       initialRoute: "/notes",
     );
--- lib/widgets/notes/notes.dart
+++ lib/widgets/notes/notes.dart
@@ -10,16 +10,28 @@ class NotesWidget extends StatelessWidget {
       appBar: AppBar(title: const Text("Записи")),
       body: ListView.separated(
           itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              title: Text('item $index'),
+            return Stack(
+              children: [
+                ListTile(
+                  title: Text('item $index'),
+                  subtitle: Text("eiowfgjweohweo wetiwert"),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: Text("16.02.22"),
+                  alignment: Alignment.centerRight,
+                )
+              ],
             );
           },
-          separatorBuilder: (BuildContext context, int index) =>
-              const Divider(),
+          separatorBuilder: (BuildContext context, int index) => const Divider(
+                color: Colors.black,
+                height: 2,
+              ),
           itemCount: 100),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
